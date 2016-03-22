d575c5d3f684f1a5160c40a45ced4e645feb90cf
Bonah <info@bonah.net>
2016-03-22 05:09:54 +0200

    Create sparkling-bonah-import.js

---
+++ inc/sparkling-bonah-import.js
@@ -0,0 +1,18 @@
+(function($) {
+    "use strict";
+        $('.bootstrapguru_import').click(function(){
+            $import_true = confirm('are you sure to import dummy content ? it will overwrite the existing data');
+            if($import_true == false) return;
+
+            $('.import_message').html(' Data is being imported please be patient, while the awesomeness is being created :)  ');
+        var data = {
+            'action': 'my_action'       
+        };
+
+       // since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
+        $.post(ajaxurl, data, function(response) {
+            $('.import_message').html('<div class="import_message_success">'+ response +'</div>');
+            //alert('Got this from the server: ' + response); <i class="fa fa-spinner fa-3x fa-spin"></i>
+        });
+    });
+})(jQuery);
